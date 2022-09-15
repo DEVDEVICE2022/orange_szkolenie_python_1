9e4bf56662d43e164c6274997001b71e6ea25563
DEVDEVICE <p.surmacz@wp.pl>
2022-09-15 16:20:07 +0200

    czwartek 16:19

--- funk1.py
+++ funk1.py
@@ -1,5 +1,31 @@
-def duzo_rzeczy(*args,**kwarks):   #funkca guma może ale nie musi przyjąc jakiś argument, przydaje się jeśl nie wiemy ile bezie argumentów
+def duzo_rzeczy(data, *args,**kwarks):   #funkca guma może ale nie musi przyjąc jakiś argument, przydaje się jeśl nie wiemy ile bezie argumentów
+    print(data)
     print(args)   #argumenty bez nazwy        ()
     print(kwarks)     #argumenty z kluczem   {}
 
 duzo_rzeczy(4,55,7.55,'Tomek',wiek=39)
+
+
+
+def maximum(*liczba):
+    if len(liczba) ==0:   # jeeli dł mojej liczby równa się ) to nic nei zwracaj
+        return None
+    else:
+        liczba_max = liczba[0]
+        for n in liczba[1:]:
+            if n>liczba_max:
+                liczba_max = n
+        return liczba_max
+
+w = maximum()
+print(w)
+w1 = maximum(3,2,8,-2,45,9,89)
+print(w1)
+
+def fun1(xy, **inne):
+    print('x:{0},y:{1},klucze w zmiennej inne:{2}'.format(x,y,list(inne.keys())))
+    inne_razem = 0
+    for k in inne.keys():
+        inne_razem=inne_razem+inne[k]
+        print('Łącznie w zmiennej inne jest wartosc {0}'.format(inne_razem))
+fun1(2,"1",liczb=34)
