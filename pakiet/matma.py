dff12da46dee931cc776dab54837fae75e5faaf8
DEVDEVICE <p.surmacz@wp.pl>
2022-09-15 14:00:34 +0200

    czwartek 13:00

---
+++ pakiet/matma.py
@@ -0,0 +1,16 @@
+#funkcje   #dogstring
+
+# funkcja/metoda. funkcja żyje w kodzie jako oddzielna część. Metoda to rzecz która jest funkcją schowaną w klasie
+#do konstrukcji funkcji używamy def
+
+# : aby funkcja mogła działać, w innych języjach klamry
+def rysunek():
+    print('''
+                <----****----->
+                <------------->
+                    menu 1
+                    menu 2         
+    ''')      # dok string to jest to co jest między potrojnymi cudzysłowami
+
+#moduł w python   new->python package
+#funkcja może byc przechowywana w liście ale bez nawiasów okrągłych
